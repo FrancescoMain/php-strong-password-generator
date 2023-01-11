02bc6d56f9c83134e9aceff2588d96ff1492e7cf
fracescoMain <cesaranofrancescomain@gmail.com>
2023-01-11 15:47:19 +0100

    aggiunto bs e titolo

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
     <?php
     session_start();
@@ -12,6 +14,19 @@
     ?>
 </head>
 <body>
+<div class="container">
+    <div class="row">
+        <div class="col text-center mt-5 mb-5">
+            <h1>Strong Password Generetor</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col text-center mb-5">
+            <h3>Genera una password sicura</h3>
+        </div>
+    </div>
+</div>
+
         <form>
             <label for="pswL">Lunghezza della password</label>
             <input type="number" name="pswL">
@@ -60,6 +75,6 @@
 
         ?>
         
-       
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html>
